44b48be3258138e71e5a99dcc50ce94d9433b0c4
Vikram Oberoi <vikram@psl.com>
2019-05-14 20:27:09 -0400

    Implement syllable count dictionary load & lookup.

--- pysyllables/__init__.py
+++ pysyllables/__init__.py
@@ -1,1 +1,26 @@
-from core import *
+import os
+
+def __load_syllable_counts():
+    filepath = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "syllable-counts.txt"
+    )
+
+    syllable_counts = {}
+
+    with open(filepath) as f:
+        for line in f:
+            word, count = line.split()
+            count = int(count)
+            syllable_counts[word] = count
+
+    return syllable_counts
+
+SYLLABLE_COUNTS = __load_syllable_counts()
+
+def get_syllable_count(word):
+    """If the dictionary contains the given word, returns the # of syllables in
+    the word. Otherwise returns None."""
+    return SYLLABLE_COUNTS.get(word.lower())
+
+__all__ = ["get_syllable_count"]
